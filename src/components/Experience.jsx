79667415e5cea4226264c927ece0dd925c11318f
adnan2508 <adnandiu2800@gmail.com>
2026-04-17 10:38:19 +0600

    soldier rendered

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,13 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Soldier } from "./Soldier";
 
 export const Experience = () => {
   return (
     <>
+      <Environment preset="sunset" />
+      <ambientLight intensity={0.3} />
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Soldier />
     </>
   );
 };
